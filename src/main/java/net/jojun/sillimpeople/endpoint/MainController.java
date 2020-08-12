cfb37e06987d4c1d1e2398c6fb7fce684e847672
jojun94 <jojun9475@gmail.com>
2020-08-13 06:39:12 +0900

    User Add Page Init

--- src/main/java/net/jojun/sillimpeople/endpoint/MainController.java
+++ src/main/java/net/jojun/sillimpeople/endpoint/MainController.java
@@ -2,7 +2,6 @@ package net.jojun.sillimpeople.endpoint;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
@@ -10,23 +9,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
-@RequestMapping(value = "/")
 public class MainController {
 
-
+    /*Thymeleaf MoV Init*/
     @GetMapping(value = "/")
     public ModelAndView home()
     {
         ModelAndView mav = new ModelAndView();
 
+        /* resources/templates 내 html 이름*/
         mav.setViewName("home");
 
         Map<String, Object> map = new HashMap<>();
         map.put("name", "jojun");
         map.put("date", LocalDateTime.now());
 
+        /*data라는 이름으로 View에 추가*/
         mav.addObject("data", map);
 
         return mav;
     }
+
+    @GetMapping(value = "/user/list")
+    public String list()
+    {
+        return "user/list";
+    }
+
 }
---
+++ src/main/resources/static/js/web/home.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('#btnModalSave').click(function(event){
+        /*ajax*/
+        console.log("Add Click Event");
+        alert("저장되었습니다!");
+    });
+
+    $('#btnSearch').click(function(event){
+        /*ajax*/
+        console.log("Search Click Event");
+    });
+
+});
