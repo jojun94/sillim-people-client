3b246ccda5cdb883654df65d3987dd94a2a7cea8
jojun94 <jojun9475@gmail.com>
2020-08-15 01:51:01 +0900

    modified

--- src/main/resources/static/js/web/home.js
+++ src/main/resources/static/js/web/home.js
@@ -2,12 +2,13 @@ $(document).ready(function(){
 
     $('#btnModalSave').click(function(event){
         /*ajax*/
-        console.log("Add Click Event");
         alert("저장되었습니다!");
+        $.ajax({
+
+        });
     });
 
     $('#btnSearch').click(function(event){
-        /*ajax*/
         console.log("Search Click Event");
     });
 
